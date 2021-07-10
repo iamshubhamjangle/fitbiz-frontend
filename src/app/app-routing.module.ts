e2d3225ff32dc28ebd2b50a37ff446878377968a
imshubham <jangleshubham@ymail.com>
2021-07-10 14:55:46 +0530

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './homescreen/home/home.component';
+import { PageNotFoundComponent } from './homescreen/page-not-found/page-not-found.component';
+import { GymComponent } from './tracking/gym/gym.component';
+import { MealComponent } from './tracking/meal/meal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'track-workout', component: GymComponent },
+  { path: 'track-meal', component: MealComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// ROUTING
+// fitbiz.com   -- home
+// fitbiz.com/track-meal
+// fitbiz.com/track-workout
+// fitbiz.com/exercises
+// fitbiz.com/profile
+// 
+// 
