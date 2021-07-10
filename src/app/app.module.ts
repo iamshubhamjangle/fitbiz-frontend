0b8bcfd1b7041a13a9f59a160246985d5d813efa
imshubham <jangleshubham@ymail.com>
2021-07-10 13:16:45 +0530

    added cards in homescreen & new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './homescreen/home/home.component';
+import { MealComponent } from './tracking/meal/meal.component';
+import { GymComponent } from './tracking/gym/gym.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    MealComponent,
+    GymComponent
   ],
   imports: [
     BrowserModule,
