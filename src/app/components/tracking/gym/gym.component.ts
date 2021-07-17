04416aa82525ac512751dd116ae38d4b64662487
imshubham <jangleshubham@ymail.com>
2021-07-17 17:52:16 +0530

    Added two way data binding

--- src/app/components/tracking/gym/gym.component.ts
+++ src/app/components/tracking/gym/gym.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from "jquery";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Workout } from 'src/app/data/workout';
 
 @Component({
   selector: 'app-gym',
@@ -9,16 +10,34 @@ import { FormsModule } from '@angular/forms';
 })
 export class GymComponent implements OnInit {
 
+  originalWorkout : Workout = {
+    id: Date.toString(),
+    date: '',
+    workoutName: '',
+    weight1: '',
+    rep1: '',
+    weight2: '',
+    rep2: '',
+    weight3: '',
+    rep3: ''
+  }
+
+  workout : Workout = { ...this.originalWorkout };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  showMyContainer: boolean = false;
+  showMyContainer: boolean = true;
   
   demoButtonClick() {
     this.showMyContainer = !this.showMyContainer;
     $('i').toggleClass('fa-chevron-down fa-chevron-up');
+  }
+
+  onSubmit(form: NgForm){
+    console.log('in onSubmit: ' + form.submitted + " form valid " + form.valid);
   }
 
 }
---
+++ src/app/data/workout.ts
@@ -0,0 +1,11 @@
+export interface Workout {
+    id: string,
+    date: string,
+    workoutName: string,
+    weight1: string,
+    weight2: string,
+    weight3: string,
+    rep1: string,
+    rep2: string,
+    rep3: string,
+}
