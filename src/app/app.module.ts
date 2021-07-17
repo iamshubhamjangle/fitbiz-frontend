f00f74d9a8b4fa6baa3fca28225469d9651bf468
imshubham <jangleshubham@ymail.com>
2021-07-17 19:09:31 +0530

    Files moved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }               from '@angular/core';
+import { BrowserModule }          from '@angular/platform-browser';
+import { FormsModule }            from '@angular/forms';
+import { HttpClientModule }       from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { GymComponent } from './components/tracking/gym/gym.component';
-import { MealComponent } from './components/tracking/meal/meal.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent }           from './app.component';
+import { AppRoutingModule }       from './app-routing.module';
+import { NavbarComponent }        from './components/navbar/navbar.component';
+import { FooterComponent }        from './components/footer/footer.component';
+import { HomeComponent }          from './components/home/home.component';
+import { GymComponent }           from './components/tracking/gym/gym.component';
+import { MealComponent }          from './components/tracking/meal/meal.component';
+import { PageNotFoundComponent }  from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
