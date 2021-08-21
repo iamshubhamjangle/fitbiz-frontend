1d8a37727f730ad81dea5d79c024821a66bcd47d
Shubham Jangle <jangleshubham13@gmail.com>
2021-08-21 16:42:06 +0530

    added error message in Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }               from '@angular/core';
 import { BrowserModule }          from '@angular/platform-browser';
 import { FormsModule }            from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS }       from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent }           from './app.component';
 import { AppRoutingModule }       from './app-routing.module';
 import { NavbarComponent }        from './components/navbar/navbar.component';
