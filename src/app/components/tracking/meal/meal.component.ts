837f2f01f5b64a0255c003a411e639595ac43b36
Shubham Jangle <jangleshubham13@gmail.com>
2021-08-29 15:21:36 +0530

    added moment

--- src/app/components/tracking/meal/meal.component.ts
+++ src/app/components/tracking/meal/meal.component.ts
@@ -6,6 +6,8 @@ import { Observable } from 'rxjs/internal/Observable';
 import { DataService } from 'src/app/data/data.service';
 import { MealData } from 'src/app/models/mealData';
 import { MealList } from 'src/app/models/mealList';
+import * as moment from 'moment';       //for formatting datetime
+
 
 @Component({
   selector: 'app-meal',
@@ -21,12 +23,15 @@ export class MealComponent implements OnInit {
   postError = false;
   postSuccess = false;
   postErrorMessage = '';
-  mealItems!: Observable<MealData[]>;
-  mealListData!: Observable<MealList[]>;
+  mealListItems!: Observable<MealList[]>;
+  mealDataItemsForUser!: Observable<MealData[]>;
 
   ngOnInit(): void {
-    this.mealItems = this.dataService.getMealItems();
-    this.mealListData = this.dataService.getMealListData();
+    this.mealListItems = this.dataService.getMealListItems();
+    this.mealDataItemsForUser = this.dataService.getMealDataItemForUser();
+    // this.mealDataItemsForUser.subscribe(val => {
+    //   console.log(val)
+    // });
   }
   
   toggleFormVisibility() {
@@ -35,19 +40,20 @@ export class MealComponent implements OnInit {
   }
 
   onSubmit(form: NgForm){
-    // console.log('in onSubmit: ' + form.submitted + " form-valid: " + form.valid);
-    // console.log('duplicate: ', this.meal);
-    // console.log("Form Selected Id: ", form.value.dropdownMealId);
+    console.log('in onSubmit: ' + form.submitted + " form-valid: " + form.valid);
+    console.log("Form Selected Id: ", form.value.dropdownMealId);
 
-    let currentDateTime = new Date();
-    var selectedMealItem = {
-      id: form.value.dropdownMealId,
+    let currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    let mealIdSelected = parseInt(form.value.dropdownMealId);
+
+    var requestBody = {
+      mealId: mealIdSelected,
       date: currentDateTime
     }
 
     if(form.valid){
       this.postError = false;
-      this.dataService.postMealForm(selectedMealItem).subscribe(
+      this.dataService.postMealForm(requestBody).subscribe(
         result => this.onPostSuccess(result),
         error => this.onHttpError(error)
         );
